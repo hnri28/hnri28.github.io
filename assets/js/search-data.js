63379191f99d4b9ca9ffe2bdb388ae9e22c88fb3
hnri28 <135322256+hnri28@users.noreply.github.com>
2026-02-27 19:13:39 +0000

    Deploying to gh-pages from @ hnri28/hnri28.github.io@6f89a334ce85f7768cb4418783768f6f9cbaa592 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv_pdf/";
+            window.location.href = "/assets/pdf/HenryChu_Feb26_2026_Resume.pdf";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
