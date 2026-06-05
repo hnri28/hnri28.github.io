08551adc1c07a27469e5f6eb426f5b0399c3c6bb
hnri28 <135322256+hnri28@users.noreply.github.com>
2026-06-05 18:46:21 +0000

    Deploying to gh-pages from @ hnri28/hnri28.github.io@de5791025ced23e6ed352fc4ee97e1d2540171f6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,9 +419,9 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-pcb-design-current-sense-amplifier-for-dc-power-supply",
+          title: 'PCB Design - Current Sense Amplifier for DC Power Supply',
+          description: "a PCB Design and fabrication project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
